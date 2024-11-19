087d1dc4003fd3e952f2f87f4eb7193d7470bf25
e-brent <enb40@pitt.edu>
2024-11-18 20:04:18 -0500

    made some progress on detail view model but nothing works yet

--- lib/view/location_detail_view.dart
+++ lib/view/location_detail_view.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 //import 'package:provider/provider.dart';
 
 
-class LocationDetailsView extends StatelessWidget {
+
+class LocationDetailsView extends StatefulWidget {
+
+  const LocationDetailsView({super.key});
+
+  @override
+  State<LocationDetailsView> createState() => _LocationDetailsViewState();
+}
+
+class _LocationDetailsViewState extends State<LocationDetailsView> {
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/widgets/study_space_details.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:academic_atlas/view_model/study_space_view_model.dart';
+
+import 'package:academic_atlas/router.dart' as LocalRouter;
+import 'package:academic_atlas/constants.dart';
+
+class StudySpaceDetails extends StatelessWidget {
+  final StudySpaceViewModel studyspace;
+
+  StudySpaceDetails({required this.studyspace});
+
+  @override
+  Widget build (BuildContext context) {
+    return Container (
+
+    );
+  }
+}
