cc04e4797201904c1862ce2551b46276cf2adb70
GraceSweeney <gds30@pitt.edu>
2024-11-18 20:01:03 -0500

    grace 2

--- lib/view/location_detail_view.dart
+++ lib/view/location_detail_view.dart
@@ -15,3 +15,4 @@ class LocationDetailsView extends StatelessWidget {
   }
 
 }
+
---
+++ lib/view/update_crowd_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:academic_atlas/view_model/study_space_view_model.dart';
+import 'package:academic_atlas/model/study_space_model.dart';
+
+class UpdateCrowdView extends StatelessWidget {
+  const UpdateCrowdView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = Provider.of<StudySpaceViewModel>(context);
+
+    return Scaffold (
+      appBar: AppBar(
+          title: Text("Update Crowd Information")
+      ),
+      body: Padding(
+        padding:const EdgeInsets.all(16.0) ,
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "How Crowded is it? "
+            ),
+            Slider(
+              value:viewModel.crowdLevel,
+              min:0,
+              max:10,
+              divisions: 10,
+              onChanged: (value){
+                //viewModel.crowdLevel(value);
+              },
+            ),
+            const Text(
+              "Choose Location Details:",
+            ),
+              Wrap(
+                spacing: 8.0,
+                runSpacing:8.0,
+                children: List.generate(
+                  viewModel.currentAmenities.length,
+                    (index)=> FilterChip(
+                      label: Text(viewModel.currentAmenities[index].amenity),
+                      selected: viewModel.currentAmenities[index].isSelected,
+                      onSelected: null,
+                    )
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Info Submitted!")),
+                  );
+                  },
+                  child: const Text("Submit"),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
