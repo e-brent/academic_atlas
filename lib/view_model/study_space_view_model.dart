f673f826ade3dfa5d9df22542f5163fa96914ce7
e-brent <enb40@pitt.edu>
2024-11-18 19:21:39 -0500

    added study spaces model, data, dataservice, and view model

---
+++ lib/view_model/study_space_view_model.dart
@@ -0,0 +1,40 @@
+import 'package:academic_atlas/model/study_space_model.dart';
+
+class StudySpaceViewModel{
+  final StudySpace studyspace;
+
+  StudySpaceViewModel({required this.studyspace});
+
+  int get id {
+    return studyspace.id;
+  }
+
+  String get name {
+    return studyspace.name;
+  }
+
+  int get location {
+    return studyspace.location;
+  }
+
+  double get crowdLevel {
+    return studyspace.crowdLevel;
+  }
+
+  void set crowdLevel(double crowd){
+    crowdLevel = (crowdLevel + crowd)/2;
+  }
+
+  List<Amenities> get currentAmenities{
+    return studyspace.currentAmenities;
+  }
+
+  void setCurrentAmenities(String amenity){
+    Amenities newAmenity = Amenities(amenity: amenity);
+    currentAmenities.add(newAmenity);
+  }
+
+  List<Amenities> get generalAmenities{
+    return studyspace.generalAmenities;
+  }
+}
